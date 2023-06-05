76c4eb0aa6daa5b5449148a8da26043c3e2d95c1
Haley Hashemi <haley.jane.hashemi@gmail.com>
2023-06-05 09:35:08 -0500

    Comments to Pishan code, modified file specificity for Scores

--- Scratch/Pishan.tcl
+++ Scratch/Pishan.tcl
@@ -1,70 +1,59 @@
 
 
-# unix_cmd will take an input file with time stamps from 0
-# and will convert topehese time stamps to unix time based on the file name
-# which is the time in GMT
-# NOTE I still need to figure out how to convert every file name to unix
 
-# Scores command works by opening each csv file in the directory, and calculating the 
-# wake/sleep/background value for every second. Each second is 25 lines in the file.
-# It then writes the value per second to a new outfile as a list. 
-# Regexp is used to identify the start time of the scoring for each file by 
-# looking for the pattern of numbers in the file name.
-# Clock scan command takes these numbers and formats them to unix time, assuming the 
-# the standard times were recorded in the GMT time zone.
-# However, these values were recorded in British Summer Time which is one hour ahead of GMT.
-# It was tricky to find the British Summer Time timezone indicator for tcl, so 
-# the unix start time is set to GMT time, and then 600s (one hour) is added to this to 
-# bring it back to BST.
+# Proc_scores opens a csv file in the directory, and
+# Proc_scores will first assign the proper unix time to each second of values, assuming that the 
+# date of the recordings is the file name in numerical "YMDHMS" form, recorded in BST time zone. 
+# This procudure assumes that a second is 25 frames, and each frame denotes 
+# a value wake, sleep, background, or unknown. Proc_Scores will then assign a 1 or 0 for each category per second, 
+# and the results will be printed to a text outfile.
 
 proc scores_cmd {fn outfile} {
 	set outfile $outfile
 	set f [open $fn r]
 	set contents [split [string trim [read $f]] \n]
 	set data [lrange $contents 1 end]
-	regexp {([0-9]+)_labels} $fn a b
-	set unixtime [clock scan $b -format %Y%m%d%H%M%S -gmt 1]
-	set unixtime [expr $unixtime + 600]
-	puts $unixtime
-	while {[llength $data] >= 25} {
-		set interval [lrange $data 0 24]
-		set data [lrange $data 25 end]
-		set wake 0
-		set sleep 0
-		set background 0
-		foreach line $interval {
-			set line [split $line "," ]
-			if {[lindex $line 3] == 1} {incr wake}
-			if {[lindex $line 2] == 1} {incr sleep}
-		} 
-		if {$wake >= 13} {
-			set wake 1
-			set sleep 0
-		} elseif {$sleep >= 13} {
-			set sleep 1
+	if { [regexp {([0-9]+)_labels} $fn a b]} {
+		set unixtime [clock scan $b -format %Y%m%d%H%M%S -gmt 1]
+		set unixtime [expr $unixtime + 600]
+		puts $unixtime
+		while {[llength $data] >= 25} {
+			set interval [lrange $data 0 24]
+			set data [lrange $data 25 end]
 			set wake 0
-		} elseif {$wake > $sleep} {
-			set wake 1
 			set sleep 0
-		} elseif {$wake < $sleep} {
-			set sleep 1
-			set wake 0
-		} else {
-			set background 1
+			set background 0
+			foreach line $interval {
+				set line [split $line "," ]
+				if {[lindex $line 3] == 1} {incr wake}
+				if {[lindex $line 2] == 1} {incr sleep}
+			} 
+			if {$wake >= 13} {
+				set wake 1
+				set sleep 0
+			} elseif {$sleep >= 13} {
+				set sleep 1
+				set wake 0
+			} elseif {$wake > $sleep} {
+				set wake 1
+				set sleep 0
+			} elseif {$wake < $sleep} {
+				set sleep 1
+				set wake 0
+			} else {
+				set background 1
+			}
+			puts $outfile "[format %.1f $unixtime] $wake $sleep $background"
+			incr unixtime
+	
 		}
-		puts $outfile "[format %.1f $unixtime] $wake $sleep $background"
-		incr unixtime
-	}
-}	
- 
+	} else {
+		puts "File $fn is not a viable file for the scores procedure."
+	}	
+ }
 
-# Fixtime procedure will take an .ndf file and assign each characteristic line a unix time stamp,
-# based on the initial unix time the recording began. 
-# Using regexp, the integers in the first value of the first line of the list are extracted and 
-# assigned to variable b
-# Then, for each line of the characteristics file, the first and second element are replaced with
-# the initial unix time plus the time elapsed in seconds since the recording began.
-# Each line is written to an outfile to create a new list.
+# Fixtime procedure will take an .ndf file and assign each characteristic line its corresponding unix time stamp,
+# Each line is written to an outfile to create a new list, with unix time, EEG, and EMG values.
 
 proc fixtime_cmd {fn out} {
 	set outfile $out 
