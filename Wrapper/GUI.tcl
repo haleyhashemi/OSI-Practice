72449ea9571dc3bc14ec79c63334373e33022339
Kevan Hashemi <hashemi@opensourceinstruments.com>
2023-06-14 10:23:40 -0400

    Updated GUI.tcl to include working console.

--- Wrapper/GUI.tcl
+++ Wrapper/GUI.tcl
@@ -61,3 +61,34 @@ button .browse -text Browse -command {
 }
 pack .browse -side left
 
+# Use the "fconfigure" command to configure the stdin channel so that it does
+# not block our process when we try to read from it. We set the blocking flag to
+# zero. We also set "buffering" to "line", even though "line-by-line" buffering
+# is the default for stdin. The stdin channel will become "readable" when a
+# complete line of text has been written into the other end of the channel by
+# the terminal or whatever external process is connected to the other end of
+# stdin. By "a complete line of text" we mean a line of text terminated with a
+# newline character.
+fconfigure stdin -blocking 0 -buffering line
+
+# When the standard input has a complete line of text ready to read out, we say
+# it has become "readable". When stdin is readable, we want to read out the line
+# and execute the command therein. We set up a "file event" to call our
+# command-handling procedure, which in this case we name "console_execute".
+fileevent stdin readable console_execute
+
+# Here is console execute, a procedure that reads the standard input, executes
+# the command at the global scope with "uplevel", prints any non-empty result
+# and catches errors.
+proc console_execute {} {
+	set cmd [read stdin]
+	if {[catch {
+		set result [uplevel $cmd]
+		if {$result != ""} {
+			puts stdout $result
+		}
+	} error_result]} {
+		puts "ERROR: $error_result"
+	}
+}
+
